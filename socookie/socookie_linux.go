d570acf1a40165e72413305d91fcb49aca5294f5
Roberto D'Auria <roberto@measurementlab.net>
2022-08-16 00:02:51 +0200

    Replace Fd() with syscall.RawConn.Control()

--- socookie/socookie_linux.go
+++ socookie/socookie_linux.go
@@ -16,20 +16,26 @@ const (
 func get(file *os.File) (uint64, error) {
 	var cookie uint64
 	cookieLen := uint32(unsafe.Sizeof(cookie))
-	// GetsockoptInt does not work for 64 bit integers, which is what the UUID is.
-	// So we crib from the GetsockoptInt implementation and ndt-server/tcpinfox,
-	// and call the syscall manually.
-	_, _, errno := syscall.Syscall6(
-		uintptr(syscall.SYS_GETSOCKOPT),
-		uintptr(int(file.Fd())),
-		uintptr(syscall.SOL_SOCKET),
-		uintptr(syscallSoCookie),
-		uintptr(unsafe.Pointer(&cookie)),
-		uintptr(unsafe.Pointer(&cookieLen)),
-		uintptr(0))
-
+	rawConn, err := file.SyscallConn()
+	if err != nil {
+		return 0, err
+	}
+	var errno syscall.Errno
+	rawConn.Control(func(fd uintptr) {
+		// GetsockoptInt does not work for 64 bit integers, which is what the UUID is.
+		// So we crib from the GetsockoptInt implementation and ndt-server/tcpinfox,
+		// and call the syscall manually.
+		_, _, errno = syscall.Syscall6(
+			uintptr(syscall.SYS_GETSOCKOPT),
+			fd,
+			uintptr(syscall.SOL_SOCKET),
+			uintptr(syscallSoCookie),
+			uintptr(unsafe.Pointer(&cookie)),
+			uintptr(unsafe.Pointer(&cookieLen)),
+			uintptr(0))
+	})
 	if errno != 0 {
-		return 0, fmt.Errorf("Error in Getsockopt. Errno=%d", errno)
+		return 0, fmt.Errorf("error in Getsockopt. Errno=%d", errno)
 	}
 	return cookie, nil
 }
